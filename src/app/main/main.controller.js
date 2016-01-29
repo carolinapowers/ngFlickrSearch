456f929e8bd73ba5989f50022cbbd67d870df842
Carolina Powers <carolinapoloni@gmail.com>
2016-01-29 00:49:29 -0500

    I can get data

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,11 +1,36 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('flickr')
-    .controller('MainController', MainController);
+    angular
+        .module('flickr')
+        .controller('MainController', MainController);
 
-  /** @ngInject */
-  function MainController($scope) {
-    
+    /** @ngInject */
+    function MainController($scope, $http) {
+
+        $scope.search = function (keyword) {
+
+            var search_tag = $scope.searchTag;
+
+            var url = 'https://api.flickr.com/services/rest';
+
+            var params = {
+                method: 'flickr.photos.search',
+                api_key: 'aff9f512a10767918225223fc710e45d',
+                tags: search_tag,
+                format: 'json',
+                nojsoncallback: 1
+            }
+
+            $http({
+                    url: url,
+                    method: 'GET',
+                    params: params
+                })
+                .then(function (response) {
+                    $scope.pictures = response.data.photos.photo;
+                    console.log($scope.pictures = response.data.photos.photo);
+                });
+        }
+    }
 })();
