13a527588320a26de0427494f350971a457b4583
Carolina Powers <carolinapoloni@gmail.com>
2016-01-29 16:54:35 -0500

    removed extra modules

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
     'use strict';
 
     angular
-        .module('flickr', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'angular-loading-bar']);
+        .module('flickr', ['ngAnimate', 'ngMessages', 'angular-loading-bar']);
 
 })();
